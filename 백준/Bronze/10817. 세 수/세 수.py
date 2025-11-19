417754cf2331b74726a4d2c93371533534d84050
yunsB <nfsodv0929@gmail.com>
2025-11-19 17:17:09 +0900

    [Bronze IV] Title: 세 수, Time: 32 ms, Memory: 32412 KB -BaekjoonHub

--- 백준/Bronze/10817. 세 수/세 수.py
+++ 백준/Bronze/10817. 세 수/세 수.py
@@ -1,3 +1,11 @@
+#선택 정렬
 nums = list(map(int,input().split()))
-nums.sort()
+
+for i in range(len(nums)):
+  min_idx = i
+  for j in range(i+1,len(nums)):
+    if nums[j] < nums[min_idx]:
+      min_idx = j
+  nums[i], nums[min_idx] = nums[min_idx], nums[i]
+
 print(nums[1])
