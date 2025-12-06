1fd0f89e94907eea9d99a4b6fc6a87d6c882bf08
Yunseop Han <nfsodv0929@gmail.com>
2025-12-06 23:00:45 +0900

    [Bronze V] Title: Hook, Time: 44 ms, Memory: 5308 KB -BaekjoonHub

---
+++ 백준/Bronze/10189. Hook/Hook.cs
@@ -0,0 +1,13 @@
+
+using System;
+
+class Program
+{
+    static void Main()
+    {
+        Console.WriteLine ("#  # #### #### #  #");
+        Console.WriteLine ("#### #  # #  # # #");
+        Console.WriteLine ("#### #  # #  # # #");
+        Console.WriteLine ("#  # #### #### #  #");
+    }
+}
