75305d8b717b2fdfe40dfc6c8b67036e7210ced0
Yunseop Han <nfsodv0929@gmail.com>
2025-12-08 02:58:28 +0900

    [Bronze V] Title: 所持金 (Money On Me), Time: 48 ms, Memory: 5468 KB -BaekjoonHub

---
+++ 백준/Bronze/33169. 所持金 （Money On Me）/所持金 （Money On Me）.cs
@@ -0,0 +1,11 @@
+using System;
+					
+public class Program
+{
+	public static void Main()
+	{
+		int A = int.Parse(Console.ReadLine());
+		int B = int.Parse(Console.ReadLine());
+		Console.WriteLine(A*1000 + B*10000);
+	}
+}
