57dccedd878adf2d74807cf183f625d29716b98a
Yunseop Han <nfsodv0929@gmail.com>
2025-12-07 16:21:37 +0900

    [Bronze III] Title: Ahmed Aly, Time: 36 ms, Memory: 32412 KB -BaekjoonHub

--- 백준/Bronze/10698. Ahmed Aly/Ahmed Aly.py
+++ 백준/Bronze/10698. Ahmed Aly/Ahmed Aly.py
@@ -1,20 +1,17 @@
 T = int(input())
 for i in range(T):
   x,op,y,eq,z = input().split()
-  X = int(x)
-  Y = int(y)
-  Z = int(z)
+  X,Y,Z = int(x),int(y),int(z)
+
   
   if op == '+':
-    if X+Y == Z:
-      print(f"Case {i+1}: YES")
-    else:
-      print(f"Case {i+1}: NO")
+    cal = X + Y
   elif op == '-':
-    if X-Y == Z:
-      print(f"Case {i+1}: YES")
-    else:
-      print(f"Case {i+1}: NO")
-
+    cal = X - Y
+  
+  if cal == Z:
+    print(f"Case {i+1}: YES")
+  else:
+    print(f"Case {i+1}: NO")
 
   
