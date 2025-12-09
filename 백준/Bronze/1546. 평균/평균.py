dbfda6d6b8c33fd6f9d9baebd2ead49421747237
Yunseop Han <nfsodv0929@gmail.com>
2025-12-09 23:07:12 +0900

    [Bronze I] Title: 평균, Time: 36 ms, Memory: 32412 KB -BaekjoonHub

--- 백준/Bronze/1546. 평균/평균.py
+++ 백준/Bronze/1546. 평균/평균.py
@@ -1,9 +1,5 @@
 N = int(input().strip()) #과목의 개수
-nowScore = list(map(int,input().split())) #현재 시험 점수 리스트
-M = max(nowScore) #최고 점수
+score = list(map(int,input().split())) #현재 시험 점수 리스트
+M = max(score) #최고 점수
 
-newScore = 0 #사기친 점수 합
-for i in range(N):
-  newScore += nowScore[i]/M*100
-
-print(newScore/N)
+print(sum(score) / M * 100 / int(N))
