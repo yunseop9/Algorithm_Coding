6645ce23554aa64b0d52bc08fc015a30ccdac485
Yunseop Han <nfsodv0929@gmail.com>
2026-04-03 18:57:38 +0900

    [Bronze II] Title: 블랙잭, Time: 84 ms, Memory: 32412 KB -BaekjoonHub

--- Python/백준/Bronze/2798. 블랙잭/블랙잭.py
+++ Python/백준/Bronze/2798. 블랙잭/블랙잭.py
@@ -1,14 +1,19 @@
+import sys
 
-N,M = map(int,input().split())
+input = sys.stdin.readline
 
-cards = list(map(int,input().split()))
+n, m = map(int, input().split())
 
-best = 0
-for i in range(N):
-  for j in range(i+1,N):
-    for k in range(j+1,N):
-      num = cards[i] + cards[j] + cards[k]
-      if num <= M and num > best:
-        best = num
+integer = list(map(int, input().split()))
 
-print(best)
+result = 0
+
+for i in range(n): 
+    for j in range(i+1,n):
+        for k in range(j+1,n):
+            total = integer[i]+integer[j]+integer[k]
+            
+            if total <= m and total > result:
+                result = total
+                
+print(result)
